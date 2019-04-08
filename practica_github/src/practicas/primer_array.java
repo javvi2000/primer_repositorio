f9d7bf7ee8c390a1b58c1a349e18a1f8a57164b5
javvi2000 <javifdzperea@gmail.com>
2019-04-08 16:17:17 +0200

    subida reciente

---
+++ practica_github/src/practicas/primer_array.java
@@ -0,0 +1,22 @@
+package practicas;
+
+public class primer_array {
+		public static void main(String args[]){
+			
+			int[] numero = new int [50];
+			
+			for (int i=0; i<numero.length; i++){
+			numero [i] = i+1;
+			}
+			
+			System.out.println("Multiplos de 10: ");
+			
+			for (int i=0; i<numero.length; i++){
+			
+				if(numero[i]%10==0){
+				System.out.println(numero[i]);
+					}
+				}
+			}
+	}
+
